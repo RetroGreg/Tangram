b0fbf4ffad4fa48f4975ac4d04925fc597d48875
RetroGreg <dupontgregoire21@gmail.com>
2024-09-25 16:03:32 +0200

    push test

---
+++ script.js
@@ -0,0 +1,114 @@
+
+
+// Le carré
+
+const button = document.querySelector('.figure1');
+
+
+const content = document.querySelector('.container');
+
+
+button.addEventListener('click', () => {
+    content.classList.toggle('carre'); 
+});
+
+
+
+// Le chat miaou
+
+const button2 = document.querySelector('.figure2');
+
+const content2 = document.querySelector('.container');
+
+button2.addEventListener('click', () => {
+    content2.classList.toggle('chat'); 
+});
+
+
+// L'oie
+
+const button3 = document.querySelector('.figure3');
+
+const content3 = document.querySelector('.container');
+
+button3.addEventListener('click', () => {
+    content3.classList.toggle('oie'); 
+});
+
+
+// L'oie verte (le canard)
+
+const button4 = document.querySelector('.figure4');
+
+const content4 = document.querySelector('.container');
+
+button4.addEventListener('click', () => {
+    content4.classList.toggle('canard'); 
+});
+
+
+// 💝
+
+const button5 = document.querySelector('.figure5');
+
+const content5 = document.querySelector('.container');
+
+button5.addEventListener('click', () => {
+    content5.classList.toggle('coeur'); 
+});
+
+
+// tortue
+
+const button6 = document.querySelector('.figure6');
+
+const content6 = document.querySelector('.container');
+
+button6.addEventListener('click', () => {
+    content6.classList.toggle('tortue'); 
+});
+
+
+
+// le bateau
+
+const button7 = document.querySelector('.figure7');
+
+const content7 = document.querySelector('.container');
+
+button7.addEventListener('click', () => {
+    content7.classList.toggle('bateau'); 
+});
+
+
+// le mystere1
+
+const button8 = document.querySelector('.figure8');
+
+const content8 = document.querySelector('.container');
+
+button8.addEventListener('click', () => {
+    content8.classList.toggle('mystere1'); 
+});
+
+
+// le mystere2
+
+const button9 = document.querySelector('.figure9');
+
+const content9 = document.querySelector('.container');
+
+button9.addEventListener('click', () => {
+    content9.classList.toggle('mystere2'); 
+});
+
+
+// le mystere3
+
+const button10 = document.querySelector('.figure10');
+
+const content10 = document.querySelector('.container');
+
+button10.addEventListener('click', () => {
+    content10.classList.toggle('mystere3'); 
+});
